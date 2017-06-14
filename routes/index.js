5e00d62a49100f43e8278c5d2a0e40641459f9f0
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-06-13 21:21:59 -0400

    update rule

--- routes/index.js
+++ routes/index.js
@@ -229,7 +229,8 @@ router.get('/refresh_token', function(req, res) {
 
   router.post('/updaterule', isLoggedIn, function(req, res) {
       var access_token = req.session.access;
-        console.log("updaterule set access test: " + JSON.stringify(req));
+      console.log("updaterule set access tes0t: " + req);
+        console.log("updaterule set access test: " + req.data);
     if(req.session.access != undefined){
       var col = req.data.col;
       var rule = req.data.rule;
