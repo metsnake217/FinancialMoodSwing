370e7e51d89d22cd01983c5969bfe4504523c86e
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-06-16 12:52:51 -0400

    redirect to mood

--- routes/index.js
+++ routes/index.js
@@ -141,7 +141,7 @@ router.get('/callback', function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         if(req.session.to != null && req.session.to.length > 0){
-          res.redirect(req.session.to+
+          res.redirect(req.session.to + '/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token,
