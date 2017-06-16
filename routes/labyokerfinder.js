93da91c45602a893891aa4ab9b5b299633c2ac31
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-06-16 11:29:33 -0400

    deleadd programmable mood

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -331,7 +331,7 @@ var vals = "", ids="";
     	console.log("t: " + t);
     	console.log("resultsids.indexOf(t) : " + resultsids.indexOf(t) );
     	if(resultsids.indexOf(t) < 0){
-    	vals += "('" +item.trackid+ "','" + playlistname + "','" + item.tracktitle + "'," + item.mode_up + "," + item.mode_down + "," + item.mode_wayup + "," + item.mode_waydown + "),"
+    	vals += "('" +item.trackid+ "','" + playlistname + "','" + item.tracktitle + "'," + item.mode_up + "," + item.mode_down + "," + item.mode_wayup + "," + item.mode_waydown + "," + item.prog_up + "," + item.prog_down + "," + item.prog_wayup + "," + item.prog_waydown + "),"
     	} else {
     		if(item.mode_up == 1){
     		updatevars_modeup += " id='" + item.trackid + "' and ";
